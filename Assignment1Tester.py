a7a40303168df673c57dcd0bd4f312bc9360a18e
minhengdey <dlminhanh272@gmail.com>
2025-06-04 20:39:16 +0700

    merge all

--- Assignment1Tester.py
+++ Assignment1Tester.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
             else:
                 print("loadratings function fail!")
 
-            [result, e] = testHelper.testrangepartition(MyAssignment, RATINGS_TABLE, 100, conn, 0, ACTUAL_ROWS_IN_INPUT_FILE)
+            [result, e] = testHelper.testrangepartition(MyAssignment, RATINGS_TABLE, 5, conn, 0, ACTUAL_ROWS_IN_INPUT_FILE)
             if result :
                 print("rangepartition function pass!")
             else:
@@ -57,7 +57,7 @@ if __name__ == '__main__':
                 print("roundrobinpartition function fail")
 
             # ALERT:: Change the partition index according to your testing sequence.
-            [result, e] = testHelper.testroundrobininsert(MyAssignment, RATINGS_TABLE, 100, 1, 3, conn, '1')
+            [result, e] = testHelper.testroundrobininsert(MyAssignment, RATINGS_TABLE, 100, 1, 3, conn, '0')
             # [result, e] = testHelper.testroundrobininsert(MyAssignment, RATINGS_TABLE, 100, 1, 3, conn, '1')
             # [result, e] = testHelper.testroundrobininsert(MyAssignment, RATINGS_TABLE, 100, 1, 3, conn, '2')
             if result :
